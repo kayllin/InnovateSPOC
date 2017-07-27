b2a0a74fd34a7d0403660b60410e2527e9f46054
JakeRong <727841270@qq.com>
2017-07-27 19:30:34 +0800

    用户界面

--- InnovateSPOC/src/com/base/Po/User.java
+++ InnovateSPOC/src/com/base/Po/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 public class User {
     
     @Id
-    private int uid;
+    private String uid;
     private String username;
     private String password;
     
@@ -16,31 +16,38 @@ public class User {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public User(int uid, String username, String password) {
+
+	public User(String uid, String username, String password) {
 		super();
 		this.uid = uid;
 		this.username = username;
 		this.password = password;
 	}
-	
-	public int getUid() {
+
+	public String getUid() {
 		return uid;
 	}
-	public void setUid(int uid) {
+
+	public void setUid(String uid) {
 		this.uid = uid;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
    
 }
