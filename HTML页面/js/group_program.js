5dceb1222b70edb81fa55635e21c8b3f8dd76a9c
zaoying1027 <1747266224@qq.com>
2017-09-09 10:02:08 +0800

    作品详情

--- HTML页面/js/group_program.js
+++ HTML页面/js/group_program.js
@@ -3,7 +3,10 @@ $("#types").click(function(){
 });
 var ContStr="";
 		for(var i=0;i<7;i++){
-			ContStr=ContStr+'<div class="content" style="background: url(../images/group_program/bg.png) no-repeat;"><div class="intro"><h1>软件工程师</h1><p>主导人：李彩</p><p>参与人：潘葛&nbsp;赵勇</p><p>类型：软件工程师</p></div></div>';
+			ContStr=ContStr+'<div class="content" style="background: url(../images/group_program/bg.png) no-repeat;"><div class="intro"><h1>软件工程师</h1><p>主导人：李彩</p><p>参与人：潘葛 赵勇</p><p>类型：软件工程师</p></div></div>';
 		}
 		$("#contents").append(ContStr);
 	
+$(document).on("click",".content",function(){
+	$("#Login").modal('show');
+});
