667eff10762885c18ac6235961164cab8c8f5a4d
zaoying1027 <1747266224@qq.com>
2017-09-14 08:51:04 +0800

    日期

--- InnovateSPOC/WebRoot/js/index.js
+++ InnovateSPOC/WebRoot/js/index.js
@@ -26,12 +26,12 @@ $(document).ready(function(){
 				var newListStr='';
 				var newGroup=data[0].length/2;
 				var newLast=data[0].length%2;
-				var newTime="";
-				var newYear="";
-				var newMothe="";
+				
 				if(newLast!==0){
 					newGroup=parseInt(newGroup)+1;
 				}
+				
+				
 				for(var i=0;i<newGroup;i++){
 					
 					if(i===0){
@@ -42,21 +42,20 @@ $(document).ready(function(){
 						newListStr=newListStr+'<li data-target="#myCarousel4" data-slide-to="'+i+'"></li>';
 					}
 					if(i!==(newGroup-1)||newLast===0){
-						
+			
 						for(var k=0;k<2;k++){
-							newTime=data[0][2*i+k].time;
-							newYear=newTime.substring(0,4);
-							newMothe=newTime.substring(5);
-							alert(data[0][2*i+k].photo);
-							newStr=newStr+'<div class="newsunit"><a href="html/news_3.html" ><img src="'+ data[0][2*i+k].photo +'" class="fl"><div class="fl news-text"><h2>'+ data[0][2*i+k].title +'</h2><p class="ct">'+ data[0][2*i+k].content +'</p></div><div class="fr news-date"><p>'+newYear+'</p><p>'+ newMothe +'</p></div><div class="news-bottom"></div></a></div>';	
+							
+							
+							/*alert(data[0][2*i+k].photo);*/
+							newStr=newStr+'<div class="newsunit"><a href="html/news_3.html" ><img src="'+ data[0][2*i+k].photo.substring(3) +'" class="fl"><div class="fl news-text"><h2>'+ data[0][2*i+k].title +'</h2><p class="ct">'+ data[0][2*i+k].content +'</p></div><div class="fr news-date"><p>'+data[0][2*i+k].time.substring(0,4)+'</p><p>'+ data[0][2*i+k].time.substring(5) +'</p></div><div class="news-bottom"></div></a></div>';	
 						}
 						newStr=newStr+'</div>';
 					}
 					else{
-						newTime=data[0][0].time.time;
-						newYear=newTime.substring(0,4);
-						newMothe=newTime.substring(5);
-						newStr=newStr+'<div class="newsunit"><a href="html/news_3.html" ><img src="'+ data[0][0].photo +'" class="fl"><div class="fl news-text"><h2>'+ data[0][0].title +'</h2><p class="ct">'+ data[0][0].title +'</p></div><div class="fr news-date"><p>'+newYear+'</p><p>'+ newMothe +'</p></div><div class="news-bottom"></div></a></div></div>';
+						
+						
+						newStr=newStr+'<div class="newsunit"><a href="html/news_3.html" ><img src="'+ data[0][0].photo.substring(3) +'" class="fl"><div class="fl news-text"><h2>'+ data[0][0].title +'</h2><p class="ct">'+ data[0][0].content +'</p></div><div class="fr news-date"><p>'+data[0][0].time.substring(0,4)+'</p><p>'+ data[0][0].time.substring(5) +'</p></div><div class="news-bottom"></div></a></div></div>';
+						/*alert(newStr);*/
 					}
 					
 					 
